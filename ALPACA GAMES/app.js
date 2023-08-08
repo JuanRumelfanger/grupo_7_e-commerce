555b73472b3c7abab7d5cbea6c7e5216cf52eaf6
GuillermoDev <joadex27@gmail.com>
2023-08-08 14:13:15 -0300

    Add page register

---
+++ ALPACA GAMES/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3002;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get(['/', '/home'], (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
