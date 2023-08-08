63bbe50af88596cff56b50c6048de6a390c85506
GuillermoDev <joadex27@gmail.com>
2023-08-08 14:20:05 -0300

    Add routing file app.js

--- ALPACA GAMES/app.js
+++ ALPACA GAMES/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3002;
-
 const app = express();
+app.use(express.static(path.resolve(__dirname, './public')));
 
-app.use(express.static('public'));
+app.listen(3000, () => {
+  console.log('Successfully started');
+});
 
-app.get(['/', '/home'], (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/productDetail', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+});
+
+app.get('/productCart', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/register.html'));
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
