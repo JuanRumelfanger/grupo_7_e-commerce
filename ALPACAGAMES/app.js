05f08ad933e493242eec9252be502fe66e405b98
JuanRumelfanger <umajuanrumel@gmail.com>
2023-08-22 20:30:29 -0300

    Mejoro el entry point

--- ALPACAGAMES/app.js
+++ ALPACAGAMES/app.js
@@ -12,23 +12,3 @@ app.use('/', mainRouter);
 app.listen(3000, () => {
   console.log('Successfully started');
 });
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/users/index.html'));
-});
-
-app.get('/productDetail', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/productDetail.html'));
-});
-
-app.get('/productCart', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/shop.html'));
-});
-
-app.get('/register', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/users/register.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/users/login.html'));
-});
