d53df5c0d2619f649b89ec90b9bb9e71913ad985
shernanr <shernanr@gmail.com>
2023-08-22 17:57:25 -0300

    fixing bugs

--- ALPACAGAMES/controllers/mainController.js
+++ ALPACAGAMES/controllers/mainController.js
@@ -2,19 +2,34 @@ const path = require('path');
 
 const mainController = {
     home: (req, res) => {
-        res.render('index');
+        res.render('index', {
+            "title": "Alpaca Games",
+            "css": "styles"
+        });
     },
     register: (req, res) => {
-        res.render('register');
+        res.render('register', {
+            "title": "Registro",
+            "css": "stylesRegister"
+        });
     },
     login: (req, res) => {
-        res.render('login');
+        res.render('login', {
+            "title": "Login",
+            "css": "stylesLogin"
+        });
     },
     shop: (req, res) => {
-        res.render('shop');
+        res.render('shop', {
+            "title": "Shop",
+            "css": "stylesShop"
+        });
     },
     productDetail: (req, res) => {
-        res.render('productDetail');
+        res.render('productDetail', {
+            "title": "Producto",
+            "css": "stylesDetails"
+        });
     },
 }
 
