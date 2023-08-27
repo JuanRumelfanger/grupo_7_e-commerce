2f007af832c4c2e84fe8ff5683fd8ba7871dad58
GuillermoDev <joadex27@gmail.com>
2023-08-27 10:06:10 -0300

    Add middleware in app.js

--- ALPACAGAMES/app.js
+++ ALPACAGAMES/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 
