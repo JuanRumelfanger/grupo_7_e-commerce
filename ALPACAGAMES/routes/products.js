3932d2658e3cba6914def117b0004a5503f13479
josue180805 <josue180805@gmail.com>
2023-09-04 23:24:04 -0300

    Agrego la ruta al detalle del producto

--- ALPACAGAMES/routes/products.js
+++ ALPACAGAMES/routes/products.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productsController = require('../controllers/productsController.js');
 
 router.get('/list', productsController.listProducts);
+router.get('/:id', productsController.productsDetail)
 
 module.exports = router;
