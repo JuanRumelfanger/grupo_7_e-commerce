c7160188baa5888fc7044e7a9b98f37155111ae7
GuillermoDev <joadex27@gmail.com>
2023-09-25 19:12:23 -0300

    Developing user entity

--- ALPACAGAMES/app.js
+++ ALPACAGAMES/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-const methodOverride =  require('method-override');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
 
 const mainRouter = require('./routes/main');
 const productsRouter = require('./routes/products');
-<<<<<<< HEAD
 const usersRouter = require('./routes/users');
-
-const cookieParser = require('cookie-parser');
-=======
->>>>>>> cf531119eb1837969d26ab4c84a48b9451db4a21
 
 const userViews = path.join(__dirname, '/views/users');
 const productsViews = path.join(__dirname, '/views/products');
@@ -20,6 +16,14 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'Secreto',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 
 app.set('view engine', 'ejs');
 app.set('views', [userViews, productsViews]);
--- ALPACAGAMES/controllers/usersController.js
+++ ALPACAGAMES/controllers/usersController.js
@@ -1,10 +1,48 @@
 const bcrypt = require('bcryptjs');
-const { body, validationResult } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
+const { validationResult } = require('express-validator');
 
-const users = {
+const dataJson = fs.readFileSync(path.join(__dirname, '../data/users.json'));
+const users = JSON.parse(dataJson);
+
+function updateUserJSON() {
+  const usersJSON = JSON.stringify(users, null, 4);
+  fs.writeFileSync(path.join(__dirname, '../data/users.json'), usersJSON);
+}
+
+const usersController = {
   login: (req, res) => {
     res.render('login');
   },
+  processLogin: (req, res) => {},
+  register: (req, res) => {
+    res.render('register');
+  },
+  processRegister: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('register', { errors: errors.errors, old: req.body });
+    } else {
+      const imageFilename = req.file ? req.file.filename : null;
+      const lastUserId = users[users.length - 1].id;
+      const newUser = {
+        id: lastUserId + 1,
+        name: req.body.firstName,
+        lastName: req.body.lastName,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        birthDate: req.body.birthDate,
+        country: req.body.country,
+        admin: false,
+        avatar: imageFilename,
+      };
+      users.push(newUser);
+      updateUserJSON();
+      res.redirect('/users/login');
+    }
+  },
 };
 
-module.exports = users;
+module.exports = usersController;
