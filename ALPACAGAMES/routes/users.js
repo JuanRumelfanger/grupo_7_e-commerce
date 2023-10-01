b267d4b9735bbd17c307aa0b2418626975760662
GuillermoDev <joadex27@gmail.com>
2023-10-01 19:38:02 -0300

    Add validation middleware

--- ALPACAGAMES/routes/users.js
+++ ALPACAGAMES/routes/users.js
@@ -3,9 +3,15 @@ const usersController = require('../controllers/usersController');
 const router = express.Router();
 const upload = require('../middlewares/registerImage.js');
 
+// Routes for users - Login and Register
 router.get('/login', usersController.login);
 router.post('/login', usersController.processLogin);
-
+// ------------------------------------------------------ //
 router.get('/register', usersController.register);
-router.post('/register', usersController.processRegister, upload.single('avatar'));
-module.exports = router; 
+router.post(
+  '/register',
+  upload.single('avatar'),
+  usersController.processRegister,
+);
+
+module.exports = router;
