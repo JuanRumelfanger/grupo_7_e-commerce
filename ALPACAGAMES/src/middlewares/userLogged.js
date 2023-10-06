e707ddc3845c06456406567843ec47459bc2fa5c
josue180805 <josue180805@gmail.com>
2023-10-06 00:18:42 -0300

    Add new arrangements to this middleware

--- ALPACAGAMES/src/middlewares/userLogged.js
+++ ALPACAGAMES/src/middlewares/userLogged.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const dataJson = fs.readFileSync(path.join(__dirname, '../data/users.json'));
+const users = JSON.parse(dataJson);
+
 function userLogged(req, res, next){
     res.locals.isLogged = false;
+
+    let emailCookie = req.cookies.userEmail
+    let userFoundCookie = users.find((user) => emailCookie == user.email);
+
+    if(userFoundCookie){
+        req.session.userAreLogged = userFoundCookie
+    }
+
     if(req.session && req.session.userAreLogged){
         res.locals.isLogged = true;
         res.locals.userAreLogged = req.session.userAreLogged;
     }
+
     next();
 }
 module.exports = userLogged;
