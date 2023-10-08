fd31388b7761fd67d7e10ddda73fd82f940fc7d7
GuillermoDev <joadex27@gmail.com>
2023-10-08 17:57:39 -0300

    Add doc and validations register

--- ALPACAGAMES/src/middlewares/question.js
+++ ALPACAGAMES/src/middlewares/question.js
@@ -1,7 +1,16 @@
-function areYouLogged(req, res, next){
-    if(req.session.userAreLogged){
-        res.redirect('/')
-    }
-    next()
+/**
+ * Middleware que verifica si el usuario ha iniciado sesión.
+ * Si el usuario ha iniciado sesión, redirige al usuario a la URL raíz.
+ * Si el usuario no ha iniciado sesión, llama a la siguiente función middleware.
+ * @param {Object} req - Objeto que representa la solicitud HTTP.
+ * @param {Object} res - Objeto que representa la respuesta HTTP.
+ * @param {Function} next - Función que llama a la siguiente función middleware.
+ */
+function areYouLogged(req, res, next) {
+  if (req.session.userAreLogged) {
+    res.redirect('/');
+  }
+  next();
 }
-module.exports = areYouLogged
+
+module.exports = areYouLogged;
--- ALPACAGAMES/src/routes/users.js
+++ ALPACAGAMES/src/routes/users.js
@@ -2,17 +2,18 @@ const express = require('express');
 const usersController = require('../controllers/usersController');
 const router = express.Router();
 const uploadFile = require('../middlewares/registerImage.js');
-const areYouLogged = require('../middlewares/question')
-const areNotYouLogged = require('../middlewares/otherQuestion')
-const validations = require('../middlewares/validation')
+const areYouLogged = require('../middlewares/question');
+const areNotYouLogged = require('../middlewares/otherQuestion');
+const validations = require('../middlewares/validation');
 // Routes for users - Login and Register
-router.get('/login', areYouLogged, validations.login,usersController.login);
+router.get('/login', areYouLogged, validations.login, usersController.login);
 router.post('/login', usersController.processLogin);
 // ------------------------------------------------------ //
 router.get('/register', areYouLogged, usersController.register);
 router.post(
   '/register',
   uploadFile.single('avatar'),
+  validations.register,
   usersController.processRegister,
 );
 
