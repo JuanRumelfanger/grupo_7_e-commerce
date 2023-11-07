014ac8305784066fb95856e5f7532640d7e7d725
Federico Gastón <fede_f018@hotmail.com>
2023-11-07 17:02:18 -0300

    uploading config in models

--- ALPACAGAMES/src/database/models/Review.js
+++ ALPACAGAMES/src/database/models/Review.js
@@ -1,14 +1,42 @@
 module.exports = (sequelize, DataTypes)=>{
-    
-    let alias = '';
+    let alias = 'Review';
     let cols = {
+        id: {
+            type: DataTypes.BIGINT(10).UNSIGNED,
+            primarykey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.BIGINT(10).UNSIGNED,
+            allowNull: false
+        }
 
     }
-    let config = {
-        tableName:'',
-       // timestamps:
-    }
-    //const .... = sequelize.define(alias, cols, config);
     
-   // return ;
+
+    let config = {
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: false
+    };
+
+
+    const Review = sequelize.define(alias,cols,config);
+
+    Review.associate = function(models) {
+        Review.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: 'user_id'
+        })
+    }
 }
