b67c868033d14f66a3d1a25a4caab026cbd7cfb2
Juan Rumelfanger <umajuanrumel@gmail.com>
2023-11-07 16:28:43 -0300

    Update WishListItems

--- ALPACAGAMES/src/database/models/WishListItems.js
+++ ALPACAGAMES/src/database/models/WishListItems.js
@@ -1,14 +1,28 @@
 module.exports = (sequelize, DataTypes)=>{
     
-    let alias = '';
+    let alias = 'WishListItems';
     let cols = {
-
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        wishListsId:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+        },
+        videoGameId:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+        }
     }
     let config = {
-        tableName:'',
-       // timestamps:
+        tableName:'type:DataTypes.INTEGER',
+        allowNull: false,
+        timestamps: false
     }
-    //const .... = sequelize.define(alias, cols, config);
     
-   // return ;
+    const WishListItems = sequelize.define(alias, cols, config);
+    
+    return WishListItems;
 }
