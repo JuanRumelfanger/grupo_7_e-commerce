a11b68dad08406bdc308b9f2399d25407a883cc4
GuillermoCruz27 <joadex27@gmail.com>
2023-11-07 17:43:55 -0300

    Refactor database models for Genre, Order, and OrderItems

--- ALPACAGAMES/src/database/models/Genre.js
+++ ALPACAGAMES/src/database/models/Genre.js
@@ -1,14 +1,21 @@
-module.exports = (sequelize, DataTypes)=>{
-    
-    let alias = '';
-    let cols = {
+module.exports = (sequelize, DataTypes) => {
+  let alias = 'Genre'
+  let cols = {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+    },
+  }
 
-    }
-    let config = {
-        tableName:'',
-       // timestamps:
-    }
-    //const .... = sequelize.define(alias, cols, config);
-    
-   // return ;
+  let config = {
+    tableName: 'genre',
+    timestamps: false,
+  }
+  const Genre = sequelize.define(alias, cols, config)
+
+  return Genre
 }
